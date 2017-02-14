02bb56795c20598b6af4e72c62561ec4919cff60
hagward <taperlawyer@gmail.com>
2017-02-14 22:46:40 +0100

    Create lexer with support for variables

---
+++ src/Lexer.java
@@ -0,0 +1,72 @@
+import java.util.ArrayList;
+import java.util.List;
+
+class Lexer {
+
+    public enum TokenType {
+        TEXT, VARIABLE
+    }
+
+    public static class Token {
+        final TokenType type;
+        final String data;
+
+        Token(TokenType type, String data) {
+            this.type = type;
+            this.data = data;
+        }
+
+        @Override
+        public String toString() {
+            return String.format("%s(%s)", type, data);
+        }
+    }
+
+    static List<Token> lex(String input) {
+        List<Token> result = new ArrayList<>();
+        for (int i = 0; i < input.length(); ) {
+            if (isStartOfVariable(input, i)) {
+                String variable = getVariable(input, i);
+                i += variable.length() + 4;
+                result.add(new Token(TokenType.VARIABLE, variable));
+            } else {
+                String text = getText(input, i);
+                i += text.length();
+                result.add(new Token(TokenType.TEXT, text));
+            }
+        }
+        return result;
+    }
+
+    private static boolean isStartOfVariable(String input, int i) {
+        return input.charAt(i) == '{' && i < input.length() - 1 && input.charAt(i + 1) == '{';
+    }
+
+    private static boolean isEndOfVariable(String input, int i) {
+        return input.charAt(i) == '}' && i < input.length() - 1 && input.charAt(i + 1) == '}';
+    }
+
+    private static String getVariable(String input, int i) {
+        int j = i;
+        for ( ; j < input.length(); ) {
+            if (isEndOfVariable(input, j)) {
+                return input.substring(i + 2, j);
+            } else {
+                j++;
+            }
+        }
+        throw new RuntimeException("Unclosed variable");
+    }
+
+    private static String getText(String input, int i) {
+        int j = i;
+        for ( ; j < input.length(); ) {
+            if (isStartOfVariable(input, j)) {
+                break;
+            } else {
+                j++;
+            }
+        }
+        return input.substring(i, j);
+    }
+}
